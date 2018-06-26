36a74ac706306e0cd4344da33a8f08b8b558c6d6
Adam McCullough <merlinfmct87@gmail.com>
2018-06-25 22:22:13 -0700

    This builds, but does not successfully execute.
    Error message:
    
    $ stack exec dabus -- --file ./dabus.dhall                                                                                           (837ms) │
    Starting Greet function.
    dabus:
    Error: Expression doesn't match annotation
    
    ./dabus.dhall : { foo : Natural, bar : Double }
    
    (input):1:1

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,48 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+
+import Data.Semigroup ((<>))
+import Dhall
+import Options.Applicative hiding (auto)
+
+data Arguments = Arguments
+  { conf_file     :: Text
+  , dry_run       :: Bool
+  }
+
+sample :: Parser Arguments
+sample = Arguments
+  <$> strOption
+    (long "file"
+    <> short 'f'
+    <> showDefault
+    <> help "Configuration file to use for dotfiles setup.")
+  <*> switch
+    (long "dry-run"
+    <> help "Dry run mode."
+    )
+
+
+data DhallConfig = DhallConfig { foo :: Natural, bar :: Double }
+  deriving (Generic, Show)
+
+instance Interpret DhallConfig
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  greet =<< execParser opts
+  where
+    opts = info (sample <**> helper)
+      ( fullDesc
+      <> progDesc "Manage setting up your dotfiles on new systems."
+      <> header "dabus -- a config management tool.")
+
+
+greet :: Arguments -> IO ()
+greet (Arguments file _) = do
+  putStrLn "Starting Greet function."
+  x <- input auto file
+  putStrLn "Read file."
+  print (x :: DhallConfig)
